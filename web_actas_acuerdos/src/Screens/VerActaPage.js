c9bcd96147648aa578b635ca1bca93907db0adf4
sophMcMor <sophyafmm@estudiantec.cr>
2023-10-15 00:41:46 -0600

    cambios ver actas

--- web_actas_acuerdos/src/Screens/VerActaPage.js
+++ web_actas_acuerdos/src/Screens/VerActaPage.js
@@ -20,19 +20,16 @@ function VerActaPage() {
   const url_archivo = location?.state?.url_archivo;
   const agenda = location?.state?.agenda;
   console.log('Acta Data', id_acta,titulo,fecha,consecutivo,palabras_clave,url_archivo,agenda);
-  if (palabras_clave) {
-    // Divide la cadena en palabras individuales utilizando un separador (por ejemplo, una coma)
-    const palabras_clave_array = palabras_clave.split(',');
-  }
-  if(fecha){
-    const fecha_new_format = formatearFecha(new Date(location?.state?.fecha));
-  }
+  console.log('Palabras Clave:', palabras_clave);
+
+  
   const formatearFecha = (fecha) => {
     const año = fecha.getFullYear();
     const mes = (fecha.getMonth() + 1).toString().padStart(2, '0');
     const día = fecha.getDate().toString().padStart(2, '0');
     return `${año}-${mes}-${día}`;
   }
+  const fecha_new_format = formatearFecha(new Date(location?.state?.fecha));
   const handleDescargarArchivo = () => {
     // Codigo para que descargue el archivo
    // Luego, crea un elemento a para el enlace de descarga
@@ -76,15 +73,13 @@ function VerActaPage() {
       try {
         const response = await axios.post('http://localhost:3001/obtener_ultima_bitacora', { id_acta });
         console.log(response.data);
-        setUltimaModif(response.data);
+        setUltimaModif(response.data[0]);
       } catch (err) {
         alert('Error al obtener última modificación: ' + err);
       }
     };
-
     fetchData();
   }, [id_acta, titulo, palabras_clave, agenda, fecha]);
-
   return (
     <div className='page'>
         <div className="navbar">
@@ -110,13 +105,11 @@ function VerActaPage() {
                 <p className='lblFecha'>Fecha: {fecha_new_format}</p>
             </div>
 
-            <p>Palabras claves:</p>
-            {palabras_clave_array.map((palabra, index) => (
-             <p className='lblPalabrasClaves' key={index}>{palabra.trim()}</p>
-             ))}
+            <p>Palabras clave:</p>
+            <p>{palabras_clave.join(', ')}</p>
 
-            <p className='lblUltimaModif'>Última modificación por: {ultimaModif}</p>
-        
+            <p className='lblUltimaModif'>Última modificación por:{ultimaModif.nombre} </p>
+
             <div className='botones'>
                 <button className="btnModificar" onClick={() => handleModificarActa(id_acta,titulo,fecha,consecutivo,palabras_clave, url_archivo,agenda)}>
                     <img className='btnModificar-icon' width="48" height="48" src="https://img.icons8.com/color/48/signing-a-document.png" alt="signing-a-document"/>
